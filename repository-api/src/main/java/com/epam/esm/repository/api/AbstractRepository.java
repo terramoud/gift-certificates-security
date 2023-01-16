0263fc297f3b2ffed17a3d43ddb71b7b14759f3d
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-17 01:50:59 +0200

    Create AbstractRepository

---
+++ repository-api/src/main/java/com/epam/esm/repository/api/AbstractRepository.java
@@ -0,0 +1,32 @@
+package com.epam.esm.repository.api;
+
+import com.epam.esm.domain.entity.AbstractEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Optional;
+
+public abstract class AbstractRepository<T extends AbstractEntity, N> implements BaseRepository<T, N> {
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+    protected Class<T> entity;
+
+    @Override
+    public Optional<T> findById(N id) {
+        T obtainedEntity = entityManager.find(entity, id);
+        return Optional.ofNullable(obtainedEntity);
+    }
+
+    @Override
+    public T save(T entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    @Override
+    public void delete(N id) {
+        T obtainedEntity = entityManager.find(entity, id);
+        entityManager.remove(obtainedEntity);
+    }
+}
