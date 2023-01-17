090d72749d1d3cf2ed64f183c978ae7664a31612
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-17 14:43:45 +0200

    Remove relationship many-to-many to list gift certificates and add lombok annotations

--- model/src/main/java/com/epam/esm/domain/entity/Tag.java
+++ model/src/main/java/com/epam/esm/domain/entity/Tag.java
@@ -1,9 +1,10 @@
 package com.epam.esm.domain.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * Represents relevant entity from database's table
@@ -13,6 +14,9 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "tags")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class Tag extends AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,73 +26,8 @@ public class Tag extends AbstractEntity {
     @Column(name = "name", unique = true)
     private String name;
 
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.PERSIST,
-            CascadeType.MERGE, CascadeType.REFRESH})
-    @JoinTable(
-            name = "certificates_tags",
-            joinColumns = @JoinColumn(name = "tag_id"),
-            inverseJoinColumns = @JoinColumn(name = "certificate_id")
-    )
-    private List<Certificate> certificates;
-
-    public Tag() {
-
-    }
-
-    public Tag(Long id, String name, List<Certificate> certificates) {
+    public Tag(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.certificates = certificates;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Certificate> getCertificates() {
-        return certificates;
-    }
-
-    public void setCertificates(List<Certificate> certificates) {
-        this.certificates = certificates;
-    }
-
-    public void addCertificateToTags(Certificate certificate) {
-        if (certificates == null) certificates = new ArrayList<>();
-        certificates.add(certificate);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tag tag = (Tag) o;
-        return id.equals(tag.id) &&
-                name.equals(tag.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Tag{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
