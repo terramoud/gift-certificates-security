2a2dea0474c92574c8f824db009f7f84c02e716c
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-18 09:32:19 +0200

    delete unused methods

--- repository-api/src/main/java/com/epam/esm/repository/api/CertificateRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/CertificateRepository.java
@@ -4,15 +4,12 @@ package com.epam.esm.repository.api;
 
 import com.epam.esm.domain.entity.Certificate;
 import org.springframework.data.domain.Pageable;
-import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.List;
 
 public interface CertificateRepository extends BaseRepository<Certificate, Long> {
+    List<Certificate> findAllCertificatesByTagId(LinkedMultiValueMap<String, String> fields, Pageable pageable, Long tagId);
 
-    List<Certificate> findAllCertificatesByTagId(MultiValueMap<String, String> fields, Pageable pageable, Long tagId);
-
-    List<Certificate> findAllCertificatesByTagName(MultiValueMap<String, String> fields, Pageable pageable, String tagName);
-
-    List<Certificate> findAllCertificatesByQuery(MultiValueMap<String, String> fields, Pageable pageable, String searchQuery);
+    List<Certificate> findAllCertificatesByTagName(LinkedMultiValueMap<String, String> fields, Pageable pageable, String tagName);
 }
