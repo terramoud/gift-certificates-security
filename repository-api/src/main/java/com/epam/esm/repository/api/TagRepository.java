f04ee02811327197ac7f3ee7979d2c3182e85a20
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-18 09:29:54 +0200

    delete not using methods

--- repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
@@ -2,14 +2,12 @@ package com.epam.esm.repository.api;
 
 import com.epam.esm.domain.entity.Tag;
 import org.springframework.data.domain.Pageable;
-import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.List;
 
 public interface TagRepository extends BaseRepository<Tag, Long> {
-    List<Tag> findAllTagsByCertificateId(MultiValueMap<String, String> fields, Pageable pageable, Long certificateId);
+    List<Tag> findAllTagsByCertificateId(LinkedMultiValueMap<String, String> fields, Pageable pageable, Long certificateId);
 
-    List<Tag> findAllTagsByCertificateName(MultiValueMap<String, String> fields, Pageable pageable, String tagName);
-
-    List<Tag> findAllTagsByQuery(MultiValueMap<String, String> fields, Pageable pageable, String searchQuery);
+    List<Tag> findAllTagsByCertificateName(LinkedMultiValueMap<String, String> fields, Pageable pageable, String certificateName);
 }
