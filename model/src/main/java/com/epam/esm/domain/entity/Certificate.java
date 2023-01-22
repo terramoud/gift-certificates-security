df6c49af73954914d0e0ff1c1a86ee0c5a91ab78
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-23 01:15:03 +0200

    update toString and hashCode to fix StackOverflowError

--- model/src/main/java/com/epam/esm/domain/entity/Certificate.java
+++ model/src/main/java/com/epam/esm/domain/entity/Certificate.java
@@ -3,12 +3,13 @@ package com.epam.esm.domain.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 /**
  * Represents relevant entity from database's table
@@ -17,13 +18,13 @@ import java.util.List;
  * @since 1.0
  */
 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@NoArgsConstructor
 @Entity
 @Table(name = "certificates")
-@Data
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
-public class Certificate extends AbstractEntity {
-    private static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+public class Certificate extends AbstractEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,20 +43,24 @@ public class Certificate extends AbstractEntity {
     @Column(name = "duration")
     private Integer duration;
 
-    @Column(name = "create_date")
+    @Column(name = "create_date", columnDefinition="TIMESTAMP(9)")
+//    @CreationTimestamp
     private LocalDateTime createDate;
 
-    @Column(name = "last_update_date")
+    @Column(name = "last_update_date", columnDefinition="TIMESTAMP(9)")
+//    @CreationTimestamp
     private LocalDateTime lastUpdateDate;
 
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.PERSIST,
-            CascadeType.MERGE, CascadeType.REFRESH})
-    @JoinTable(
-            name = "certificates_tags",
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE})
+    @JoinTable(name = "certificates_tags",
             joinColumns = @JoinColumn(name = "certificate_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
-    private List<Tag> tags;
+    private Set<Tag> tags;
 
     public Certificate(Long id,
                        String name,
@@ -64,7 +69,7 @@ public class Certificate extends AbstractEntity {
                        Integer duration,
                        LocalDateTime createDate,
                        LocalDateTime lastUpdateDate,
-                       List<Tag> tags) {
+                       Set<Tag> tags) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -75,9 +80,20 @@ public class Certificate extends AbstractEntity {
         this.tags = tags;
     }
 
-    public void addTagsToCertificate(Tag tag) {
-        if (tags == null) tags = new ArrayList<>();
-        tags.add(tag);
+    public Certificate(Long id,
+                       String name,
+                       String description,
+                       BigDecimal price,
+                       Integer duration,
+                       LocalDateTime createDate,
+                       LocalDateTime lastUpdateDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.createDate = createDate;
+        this.lastUpdateDate = lastUpdateDate;
     }
 }
 
