728f872a851074405fcf7ed7bdf0ff4ba1fe9667
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-23 01:15:20 +0200

    update toString and hashCode to fix StackOverflowError

--- model/src/main/java/com/epam/esm/domain/entity/Tag.java
+++ model/src/main/java/com/epam/esm/domain/entity/Tag.java
@@ -3,8 +3,11 @@ package com.epam.esm.domain.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Represents relevant entity from database's table
@@ -12,12 +15,13 @@ import javax.persistence.*;
  * @author Oleksadr Koreshev
  * @since 1.0
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@NoArgsConstructor
 @Entity
 @Table(name = "tags")
-@Data
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
-public class Tag extends AbstractEntity {
+public class Tag extends AbstractEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -26,8 +30,19 @@ public class Tag extends AbstractEntity {
     @Column(name = "name", unique = true)
     private String name;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(mappedBy = "tags", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Certificate> certificates;
+
     public Tag(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Tag(Long id, String name, Set<Certificate> certificates) {
+        this.id = id;
+        this.name = name;
+        this.certificates = certificates;
     }
 }
