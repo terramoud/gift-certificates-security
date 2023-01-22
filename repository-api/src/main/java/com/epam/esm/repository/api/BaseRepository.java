808418a013eb7f72de3823b1ba644040ca1e1c5c
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-23 01:10:45 +0200

    add method update

--- repository-api/src/main/java/com/epam/esm/repository/api/BaseRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/BaseRepository.java
@@ -2,14 +2,16 @@ package com.epam.esm.repository.api;
 
 import com.epam.esm.domain.entity.AbstractEntity;
 import org.springframework.data.domain.Pageable;
-import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public interface BaseRepository<E extends AbstractEntity, N> {
-    List<E> findAll(MultiValueMap<String, String> fields, Pageable pageable);
+    List<E> findAll(LinkedMultiValueMap<String, String> fields, Pageable pageable);
     Optional<E> findById(N id);
     E save(E entity);
-    void delete(N id);
+    E update(E entity, N id);
+    void delete(E entity);
 }
