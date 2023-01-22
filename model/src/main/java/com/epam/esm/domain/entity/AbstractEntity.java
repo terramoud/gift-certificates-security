fa632d2e2d915647bbc748c51d99c8aeb945f24b
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-23 01:11:01 +0200

    fix AbstractEntity

--- model/src/main/java/com/epam/esm/domain/entity/AbstractEntity.java
+++ model/src/main/java/com/epam/esm/domain/entity/AbstractEntity.java
@@ -1,5 +1,10 @@
 package com.epam.esm.domain.entity;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
@@ -8,6 +13,9 @@ import java.io.Serializable;
  * @author Oleksadr Koreshev
  * @since 1.0
  */
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
 public abstract class AbstractEntity implements Serializable {
     private static final long serialVersionUID = -1491456600908233143L;
 }
