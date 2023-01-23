b48629e8d03cde5a3fc70e815b1d540e61560544
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-23 14:44:24 +0200

    remove unused methods

--- repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
@@ -1,18 +1,8 @@
 package com.epam.esm.repository.api;
 
 import com.epam.esm.domain.entity.Tag;
-import org.springframework.data.domain.Pageable;
-import org.springframework.util.LinkedMultiValueMap;
-
-import java.util.List;
 
 
 public interface TagRepository extends BaseRepository<Tag, Long> {
-    List<Tag> findAllTagsByCertificateId(LinkedMultiValueMap<String, String> fields,
-                                         Pageable pageable,
-                                         Long certificateId);
 
-    List<Tag> findAllTagsByCertificateName(LinkedMultiValueMap<String, String> fields,
-                                           Pageable pageable,
-                                           String certificateName);
 }
