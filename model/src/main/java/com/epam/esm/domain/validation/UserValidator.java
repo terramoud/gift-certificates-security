3c268532c6b5794ec7415e12721c840bac173fbe
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-24 11:35:05 +0200

    create UserValidator

--- model/src/main/java/com/epam/esm/domain/validation/UserValidator.java
+++ model/src/main/java/com/epam/esm/domain/validation/UserValidator.java
@@ -3,19 +3,17 @@ package com.epam.esm.domain.validation;
 import com.epam.esm.domain.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-public class UserValidator {
+@Component
+public class UserValidator extends EntityValidator {
     private static final Logger LOG = LogManager.getLogger(UserValidator.class);
     private static final String EMAIL_REGEXP = "^\\w+([.-]?\\w+){0,249}@\\w+([.-]?\\w+){0,249}(\\.\\w{2,3}){1,249}$";
     private static final String LOGIN_REGEXP = "^[\\p{L}][\\p{L}0-9]{2,31}$";
     private static final String PASSWORD_REGEXP = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,31}$";
-
-    public boolean validateId(long userId) {
-        return userId >= 0 && userId <= Integer.MAX_VALUE;
-    }
 
     public boolean validateRoleId(int roleId) {
         return roleId > 0;
