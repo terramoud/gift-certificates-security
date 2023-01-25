edb469cf8abd6be9f2bbaa092f5418c16f7e24d7
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-25 07:13:38 +0200

    implement getUserById() method

--- service-api/src/main/java/com/epam/esm/service/api/UserService.java
+++ service-api/src/main/java/com/epam/esm/service/api/UserService.java
@@ -1,5 +1,6 @@
 package com.epam.esm.service.api;
 
+import com.epam.esm.domain.entity.Tag;
 import com.epam.esm.domain.entity.User;
 import org.springframework.util.LinkedMultiValueMap;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers(LinkedMultiValueMap<String, String> fields, int size, int page);
+
+    User getUserById(Long id);
 }
--- service-impl/src/main/java/com/epam/esm/service/impl/UserServiceImpl.java
+++ service-impl/src/main/java/com/epam/esm/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.epam.esm.service.impl;
 
 import com.epam.esm.domain.entity.User;
+import com.epam.esm.domain.validation.UserValidator;
 import com.epam.esm.exceptions.ErrorCodes;
 import com.epam.esm.exceptions.InvalidPaginationParameterException;
+import com.epam.esm.exceptions.InvalidResourcePropertyException;
+import com.epam.esm.exceptions.ResourceNotFoundException;
 import com.epam.esm.repository.api.UserRepository;
 import com.epam.esm.service.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +22,13 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserValidator validator;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,
+                           UserValidator validator) {
         this.userRepository = userRepository;
+        this.validator = validator;
     }
 
     @Override
@@ -35,5 +41,14 @@ public class UserServiceImpl implements UserService {
                 "offset", page + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
         Pageable pageRequest = PageRequest.of(page, size);
         return userRepository.findAll(fields, pageRequest);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        if (!validator.validateId(id)) throw new InvalidResourcePropertyException(
+                "user.error.invalid.id", id, ErrorCodes.INVALID_USER_ID_PROPERTY);
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "user.not.found" , id, ErrorCodes.NOT_FOUND_USER_RESOURCE));
     }
 }
