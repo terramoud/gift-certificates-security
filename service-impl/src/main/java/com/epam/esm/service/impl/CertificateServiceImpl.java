b4df56467251031568d35bddd147e547efc2ca48
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-29 17:14:04 +0200

    refactor to use Java API for bean validation

--- service-impl/src/main/java/com/epam/esm/service/impl/CertificateServiceImpl.java
+++ service-impl/src/main/java/com/epam/esm/service/impl/CertificateServiceImpl.java
@@ -1,150 +1,120 @@
 package com.epam.esm.service.impl;
 
+import com.epam.esm.domain.converter.CertificateDtoConverter;
+import com.epam.esm.domain.converter.TagDtoConverter;
 import com.epam.esm.domain.entity.Certificate;
-import com.epam.esm.domain.validation.CertificateValidator;
-import com.epam.esm.domain.validation.TagValidator;
+import com.epam.esm.domain.entity.Tag;
+import com.epam.esm.domain.payload.CertificateDto;
+import com.epam.esm.domain.payload.PageDto;
+import com.epam.esm.domain.validation.OnCreate;
 import com.epam.esm.exceptions.*;
 import com.epam.esm.repository.api.CertificateRepository;
 import com.epam.esm.repository.api.TagRepository;
 import com.epam.esm.service.api.CertificateService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.validation.annotation.Validated;
 
-import java.math.BigDecimal;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Data
 @Service
 @Transactional
+@Validated
 public class CertificateServiceImpl implements CertificateService {
 
+    private static final String TAG_NAME_REGEXP = "^[\\p{L}][\\p{L} \\-']{0,30}[\\p{L}]$";
     private static final String CERTIFICATE_NOT_FOUND = "certificate.not.found";
-    private static final String CERTIFICATE_ERROR_INVALID_ID = "certificate.error.invalid.id";
+    private static final String CERTIFICATE_ID_NOT_MAPPED = "certificate.id.not.mapped";
+    private static final String WRONG_TAG_NAME = "wrong.tag.name";
+    private static final String WRONG_CERTIFICATE_ID = "wrong.certificate.id";
+    private static final String WRONG_TAG_ID = "wrong.tag.id";
+
     private final CertificateRepository certificateRepository;
-    private final CertificateValidator validator;
-    private final TagValidator tagValidator;
+    private final CertificateDtoConverter converter;
     private final TagRepository tagRepository;
+    private final TagDtoConverter tagConverter;
 
-    @Autowired
-    public CertificateServiceImpl(CertificateRepository certificateRepository,
-                                  CertificateValidator certificateValidator,
-                                  TagValidator tagValidator,
-                                  TagRepository tagRepository) {
-        this.certificateRepository = certificateRepository;
-        this.validator = certificateValidator;
-        this.tagValidator = tagValidator;
-        this.tagRepository = tagRepository;
+    @Override
+    public List<CertificateDto> findAll(LinkedMultiValueMap<String, String> fields, @Valid PageDto pageDto) {
+        Pageable pageRequest = PageRequest.of(pageDto.getPage(), pageDto.getSize());
+        return converter.toDto(certificateRepository.findAll(fields, pageRequest));
     }
 
     @Override
-    public List<Certificate> getAllCertificates(LinkedMultiValueMap<String, String> fields,
-                                                int size,
-                                                int page) {
-        if (size < 1) throw new InvalidPaginationParameterException(
-                "size", size + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        if (page < 0) throw new InvalidPaginationParameterException(
-                "page", page + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        Pageable pageRequest = PageRequest.of(page, size);
-        return certificateRepository.findAll(fields, pageRequest);
+    public List<CertificateDto> findAllByTagId(LinkedMultiValueMap<String, String> fields,
+                                               @Valid PageDto pageDto,
+                                               @Positive(message = WRONG_TAG_ID) Long id) {
+        Pageable pageRequest = PageRequest.of(pageDto.getPage(), pageDto.getSize());
+        return converter.toDto(certificateRepository.findAllCertificatesByTagId(fields, pageRequest, id));
     }
 
     @Override
-    public List<Certificate> getAllCertificatesByTagId(LinkedMultiValueMap<String, String> fields,
-                                                       int size,
-                                                       int page,
-                                                       Long tagId) {
-        if (!tagValidator.validateId(tagId)) throw new InvalidResourcePropertyException(
-                "tag.error.invalid.id", tagId, ErrorCodes.INVALID_TAG_ID_PROPERTY);
-        if (size < 1) throw new InvalidPaginationParameterException(
-                "size", size + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        if (page < 0) throw new InvalidPaginationParameterException(
-                "page", page + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        Pageable pageRequest = PageRequest.of(page, size);
-        return certificateRepository.findAllCertificatesByTagId(fields, pageRequest, tagId);
+    public List<CertificateDto> findAllByTagName(LinkedMultiValueMap<String, String> fields,
+                                                 @Valid PageDto pageDto,
+                                                 @Pattern(regexp = TAG_NAME_REGEXP,
+                                                         message = WRONG_TAG_NAME) String tagName) {
+        Pageable pageRequest = PageRequest.of(pageDto.getPage(), pageDto.getSize());
+        return converter.toDto(certificateRepository.findAllCertificatesByTagName(fields, pageRequest, tagName));
     }
 
     @Override
-    public List<Certificate> getAllCertificatesByTagName(LinkedMultiValueMap<String, String> fields,
-                                                         int size,
-                                                         int page,
-                                                         String tagName) {
-        if (!tagValidator.validateName(tagName)) throw new InvalidResourceNameException(
-                    "tag.error.invalid.name", tagName, ErrorCodes.INVALID_TAG_NAME_PROPERTY);
-        if (size < 1) throw new InvalidPaginationParameterException(
-                "size", size + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        if (page < 0) throw new InvalidPaginationParameterException(
-                "page", page + "", ErrorCodes.INVALID_PAGINATION_PARAMETER);
-        Pageable pageRequest = PageRequest.of(page, size);
-        return certificateRepository.findAllCertificatesByTagName(fields, pageRequest, tagName);
-    }
-
-    @Override
-    public Certificate getCertificateById(Long id) {
-        if (!validator.validateId(id)) throw new InvalidResourcePropertyException(
-                CERTIFICATE_ERROR_INVALID_ID, id, ErrorCodes.INVALID_CERTIFICATE_ID_PROPERTY);
-        return certificateRepository.findById(id)
+    public CertificateDto findById(@Positive(message = WRONG_CERTIFICATE_ID) Long id) {
+        return converter.toDto(certificateRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(
-                        CERTIFICATE_NOT_FOUND, id, ErrorCodes.NOT_FOUND_CERTIFICATE_RESOURCE));
+                        CERTIFICATE_NOT_FOUND, id, ErrorCodes.NOT_FOUND_CERTIFICATE_RESOURCE)));
+    }
+
+    @Validated(OnCreate.class)
+    @Override
+    public CertificateDto create(@Valid CertificateDto certificateDto) {
+        Certificate certificate = converter.toEntity(certificateDto);
+        return converter.toDto(certificateRepository.save(certificate));
     }
 
     @Override
-    public Certificate addCertificate(Certificate certificate) {
-        String name = certificate.getName();
-        String description = certificate.getDescription();
-        BigDecimal price = certificate.getPrice();
-        Integer duration = certificate.getDuration();
-        if (!validator.validateName(name)) throw new InvalidResourceNameException(
-                    "certificate.error.invalid.name", name, ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validateBigTextField(description)) throw new InvalidResourcePropertyException(
-                    "certificate.error.invalid.description", description, ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validatePrice(price)) throw new InvalidResourcePropertyException(
-                    "certificate.error.invalid.price", price + "", ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validateDuration(duration)) throw new InvalidResourcePropertyException(
-                    "certificate.error.invalid.duration", duration + "", ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        return certificateRepository.save(certificate);
+    public CertificateDto deleteById(@Positive(message = WRONG_CERTIFICATE_ID) Long id) {
+        CertificateDto certificateDto = findById(id);
+        certificateRepository.delete(converter.toEntity(certificateDto));
+        return certificateDto;
     }
 
     @Override
-    public Certificate updateCertificateById(Long id, Certificate certificate) {
-        if (!validator.validateId(id)) throw new InvalidResourcePropertyException(
-                CERTIFICATE_ERROR_INVALID_ID, id, ErrorCodes.INVALID_CERTIFICATE_ID_PROPERTY);
-        if (!validator.validateId(id) || !certificate.getId().equals(id)) throw new InvalidResourcePropertyException(
-                CERTIFICATE_ERROR_INVALID_ID, id, ErrorCodes.INVALID_CERTIFICATE_ID_PROPERTY);
-        Certificate certificateToUpdate = getCertificateById(id);
-        if (certificate.getName() != null) certificateToUpdate.setName(certificate.getName());
-        if (certificate.getDescription() != null) certificateToUpdate.setDescription(certificate.getDescription());
-        if (certificate.getPrice() != null) certificateToUpdate.setPrice(certificate.getPrice());
-        if (certificate.getDuration() != null) certificateToUpdate.setDuration(certificate.getDuration());
-        if (certificate.getTags() != null) certificateToUpdate.addTags(certificate.getTags());
-        String name = certificateToUpdate.getName();
-        String description = certificateToUpdate.getDescription();
-        BigDecimal price = certificateToUpdate.getPrice();
-        Integer duration = certificateToUpdate.getDuration();
-        if (!validator.validateName(name)) throw new InvalidResourceNameException(
-                "certificate.error.invalid.name", name, ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validateName(description)) throw new InvalidResourcePropertyException(
-                "certificate.error.invalid.description", description, ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validatePrice(price)) throw new InvalidResourcePropertyException(
-                "certificate.error.invalid.price", price + "", ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        if (!validator.validateDuration(duration)) throw new InvalidResourcePropertyException(
-                "certificate.error.invalid.duration", duration + "", ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-        certificateToUpdate.getTags().forEach(tag -> {
-            if (!tagValidator.validate(tag)) throw new InvalidResourcePropertyException(
-                "certificate.error.invalid.joined.tag", tag.getId(), ErrorCodes.INVALID_CERTIFICATE_PROPERTY);
-            tagRepository.findById(tag.getId())
-                    .orElseThrow(() -> new ResourceNotFoundException(
-                            "tag.error.update.not.found", tag.getId(), ErrorCodes.NOT_FOUND_TAG_RESOURCE));
-        });
-        return certificateRepository.update(certificateToUpdate, id);
+    public CertificateDto update(@Positive(message = WRONG_CERTIFICATE_ID) Long id,
+                                 @Valid CertificateDto certificateDto) {
+        if (!isCertificateIdEqualsRequestId(certificateDto.getId(), id)) {
+            throw new InvalidResourcePropertyException(CERTIFICATE_ID_NOT_MAPPED, id, ErrorCodes.INVALID_ID_PROPERTY);
+        }
+        Certificate certificateToUpdate = converter.toEntity(findById(id));
+        Set<Tag> tagsToUpdate = tagConverter.toEntity(certificateDto.getTags());
+        certificateToUpdate.addTags(getUniqueTagsForThisCertificate(certificateToUpdate.getTags(), tagsToUpdate));
+        updateTags(tagsToUpdate);
+        return converter.toDto(certificateRepository.update(certificateToUpdate, id));
     }
 
-    @Override
-    public Certificate deleteCertificateById(Long id) {
-        Certificate certificate = getCertificateById(id);
-        certificateRepository.delete(certificate);
-        return certificate;
+    private Set<Tag> getUniqueTagsForThisCertificate(Set<Tag> sourceTags, Set<@Valid Tag> tagsToUpdate) {
+        Set<Long> sourceTagsIds = sourceTags.stream()
+                .map(Tag::getId)
+                .collect(Collectors.toSet());
+        return tagsToUpdate.stream()
+                .filter(tag -> !sourceTagsIds.contains(tag.getId()))
+                .collect(Collectors.toSet());
+    }
+
+    private void updateTags(Set<@Valid Tag> tagsToUpdate) {
+        tagsToUpdate.forEach(tag -> tagRepository.update(tag, tag.getId()));
+    }
+
+    private boolean isCertificateIdEqualsRequestId(Long certificateId, Long requestId) {
+        return certificateId.equals(requestId);
     }
 }
