a6128b95458e50feda84e73c7d002677e06ad691
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-29 10:28:24 +0200

    add additional fields

--- model/src/main/java/com/epam/esm/domain/payload/UserDto.java
+++ model/src/main/java/com/epam/esm/domain/payload/UserDto.java
@@ -6,14 +6,18 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.validation.constraints.Email;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class UserDto extends RepresentationModel<UserDto> {
-
     private Long id;
-
     private String login;
 
+    @Email
+    private String email;
+    private String password;
+    private int roleId;
 }
