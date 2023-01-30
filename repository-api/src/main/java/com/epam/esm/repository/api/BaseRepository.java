2ea9574cddecc0592b7415cce1d8d6fe524dcd4c
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-30 07:14:35 +0200

    add default createLikeQuery() method

--- repository-api/src/main/java/com/epam/esm/repository/api/BaseRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/BaseRepository.java
@@ -4,14 +4,21 @@ import com.epam.esm.domain.entity.AbstractEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.LinkedMultiValueMap;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 
 
 public interface BaseRepository<E extends AbstractEntity, N> {
+    String SQL_LIKE_PATTERN = "%{0}%";
+
     List<E> findAll(LinkedMultiValueMap<String, String> fields, Pageable pageable);
     Optional<E> findById(N id);
     E save(E entity);
     E update(E entity, N id);
     void delete(E entity);
+
+    default String createLikeQuery(String searchQuery) {
+        return MessageFormat.format(SQL_LIKE_PATTERN, searchQuery);
+    }
 }
