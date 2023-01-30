cc1c5635b092daa99ac03c489a975d59484eca0f
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-30 09:23:55 +0200

    fix logger and remove stack traces and convert all string messages to constants

--- exception/src/main/java/com/epam/esm/exceptions/GlobalRestExceptionHandler.java
+++ exception/src/main/java/com/epam/esm/exceptions/GlobalRestExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.epam.esm.exceptions;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -18,11 +21,12 @@ import org.springframework.web.util.WebUtils;
 
 import java.sql.SQLException;
 
-import static com.epam.esm.exceptions.ErrorCodes.METHOD_ARGUMENT_CONSTRAINT_VIOLATION;
-import static com.epam.esm.exceptions.ErrorCodes.SUFFIX_RESPONSE_ENTITY_EXCEPTIONS;
+import static com.epam.esm.exceptions.ErrorCodes.*;
+import static com.epam.esm.exceptions.ExceptionConstants.*;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
 
 
@@ -31,91 +35,91 @@ public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Throwable.class)
     public final ResponseEntity<ApiErrorResponse> handleAllExceptionsAndErrors(Throwable ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorCode(ErrorCodes.INTERNAL_SERVER_ERROR.stringCode());
-        apiErrorResponse.setErrorMessage("internal.server.error");
+        apiErrorResponse.setErrorCode(INTERNAL_SERVER_ERROR.stringCode());
+        apiErrorResponse.setErrorMessage(SERVER_ERROR_500);
         if (ex.getMessage() != null && ex.getMessage().startsWith("get null list resources")) {
-            apiErrorResponse.setErrorMessage(translator.toLocale(ex.getMessage().replace(
-                    "get null list resources", "get.null.list.resources")));
-            apiErrorResponse.setErrorCode(ErrorCodes.NULL_INSTEAD_LIST.stringCode());
+            apiErrorResponse.setErrorCode(NULL_INSTEAD_LIST.stringCode());
+            apiErrorResponse.setErrorMessage(translator.toLocale(ex.getMessage()
+                    .replace("get null list resources", NULL_INSTEAD_LIST_RESOURCES)));
         }
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(DataAccessException.class)
     public final ResponseEntity<ApiErrorResponse> handleDataAccessException(DataAccessException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorCode(ErrorCodes.DATA_ACCESS_EXCEPTION.stringCode());
-        apiErrorResponse.setErrorMessage("data.access.exception");
+        apiErrorResponse.setErrorCode(DATA_ACCESS_EXCEPTION.stringCode());
+        apiErrorResponse.setErrorMessage(DATA_ACCESS_CONSTRAINT);
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public final ResponseEntity<ApiErrorResponse> handleDataAccessException(DataIntegrityViolationException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorCode(ErrorCodes.DATA_INTEGRITY_VIOLATION.stringCode());
-        apiErrorResponse.setErrorMessage("data.integrity.violation");
+        apiErrorResponse.setErrorCode(DATA_INTEGRITY_VIOLATION.stringCode());
+        apiErrorResponse.setErrorMessage(VIOLATION_DATA_INTEGRITY);
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
     public final ResponseEntity<ApiErrorResponse> handleDuplicateKey(DuplicateKeyException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorMessage(translator.toLocale("mysql.error.duplicate.column"));
-        apiErrorResponse.setErrorCode(ErrorCodes.SQL_DUPLICATE_ENTRY.stringCode());
+        apiErrorResponse.setErrorMessage(translator.toLocale(MYSQL_ERROR_DUPLICATE_COLUMN));
+        apiErrorResponse.setErrorCode(SQL_DUPLICATE_ENTRY.stringCode());
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SQLException.class)
     protected ResponseEntity<ApiErrorResponse> customerNotFound(SQLException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorCode(ErrorCodes.SQL_ERROR.stringCode());
-        apiErrorResponse.setErrorMessage("sql.error.default.message");
+        apiErrorResponse.setErrorCode(SQL_ERROR.stringCode());
+        apiErrorResponse.setErrorMessage(SQL_ERROR_DEFAULT_MESSAGE);
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceUnsupportedOperationException.class)
     protected ResponseEntity<ApiErrorResponse> customerNotFound(ResourceUnsupportedOperationException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-        apiErrorResponse.setErrorCode(ErrorCodes.DATA_ACCESS_EXCEPTION.stringCode());
+        apiErrorResponse.setErrorCode(DATA_ACCESS_EXCEPTION.stringCode());
         apiErrorResponse.setErrorMessage(translator.toLocale(ex.getMessage()));
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
         apiErrorResponse.setErrorCode(METHOD_ARGUMENT_CONSTRAINT_VIOLATION.stringCode());
         apiErrorResponse.setErrorMessage(messageFormatter.getLocalizedMessage(ex));
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @NonNull
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex,
-            @Nullable Object body,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
-        ex.printStackTrace();
+    protected ResponseEntity<Object> handleExceptionInternal(@NonNull Exception ex,
+                                                             @Nullable Object body,
+                                                             @NonNull HttpHeaders headers,
+                                                             @NonNull HttpStatus status,
+                                                             @NonNull WebRequest request) {
+        log.warn(ex.getMessage(), ex);
         if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, RequestAttributes.SCOPE_REQUEST);
         }
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
         apiErrorResponse.setErrorMessage(ex.getMessage());
         apiErrorResponse.setErrorCode(status.value() + SUFFIX_RESPONSE_ENTITY_EXCEPTIONS.stringCode());
         if (ex instanceof NoHandlerFoundException && ex.getMessage().startsWith("No handler found for")) {
             status = HttpStatus.NOT_FOUND;
-            apiErrorResponse.setErrorCode(ErrorCodes.NO_HANDLER_FOUND.stringCode());
-            apiErrorResponse.setErrorMessage(ex.getMessage().replace(
-                    "No handler found for", translator.toLocale("no.handler.found.for")));
+            apiErrorResponse.setErrorCode(NO_HANDLER_FOUND.stringCode());
+            apiErrorResponse.setErrorMessage(ex.getMessage()
+                    .replace("No handler found for", translator.toLocale(NO_HANDLER_FOUND_FOR)));
         }
         return new ResponseEntity<>(apiErrorResponse, headers, status);
     }
@@ -146,23 +150,11 @@ public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     protected ResponseEntity<ApiErrorResponse> handleResourceException(ResourceException ex, HttpStatus httpStatus) {
-        ex.printStackTrace();
+        log.warn(ex.getMessage(), ex);
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
         apiErrorResponse.setErrorCode(ex.getErrorCode());
-        apiErrorResponse.setErrorMessage(translator.toLocale(ex.getMessage()) + " " + ex.getDetails());
-        return new ResponseEntity<>(apiErrorResponse, httpStatus);
-    }
-
-    @ExceptionHandler(InvalidPaginationParameterException.class)
-    public final ResponseEntity<ApiErrorResponse> handleInvalidInputParameter(InvalidPaginationParameterException ex) {
-        ex.printStackTrace();
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
         apiErrorResponse.setErrorMessage(
-                translator.toLocale("error.invalid.pagination.parameter")
-                        .concat(" " + ex.getParameterName())
-                        .concat(translator.toLocale("cannot.be"))
-                        .concat(" " + ex.getParameterValue()));
-        apiErrorResponse.setErrorCode(ex.getErrorCode());
-        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+                String.format("%s %s", translator.toLocale(ex.getMessage()), ex.getDetails()));
+        return new ResponseEntity<>(apiErrorResponse, httpStatus);
     }
 }
