2c7ada0426c528a55aeb9928e8915dc93d68a93b
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-02-04 22:39:13 +0200

    add findMostPopularTagOfUserWithHighestCostOfAllOrders

--- repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
+++ repository-api/src/main/java/com/epam/esm/repository/api/TagRepository.java
@@ -2,7 +2,9 @@ package com.epam.esm.repository.api;
 
 import com.epam.esm.domain.entity.Tag;
 
+import java.util.Optional;
+
 
 public interface TagRepository extends BaseRepository<Tag, Long> {
-
+    Optional<Tag> findMostPopularTagOfUserWithHighestCostOfAllOrders();
 }
