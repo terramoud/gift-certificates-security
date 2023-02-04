44dbb0618f0021befc765e344750d6a4017d4e48
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-02-04 22:42:53 +0200

    add findMostPopularTagOfUserWithHighestCostOfAllOrders

--- repository-impl/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
+++ repository-impl/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.epam.esm.repository.impl;
 
+import com.epam.esm.domain.entity.Order;
+import com.epam.esm.domain.entity.Certificate;
 import com.epam.esm.domain.entity.Tag;
 import com.epam.esm.repository.api.TagRepository;
 import lombok.NoArgsConstructor;
@@ -24,12 +26,11 @@ public class TagRepositoryImpl implements TagRepository {
     public static final String SORT_REQUEST_PARAM = "sort";
     public static final String SEARCH_REQUEST_PARAM = "search";
 
-    private final Map<String, BiFunction<CriteriaBuilder, Root<Tag>, Order>> sortBy = Map.of(
-            "+name", (cb, root) -> cb.asc(root.get(TAG_NAME)),
-            "-name", (cb, root) -> cb.desc(root.get(TAG_NAME)),
-            "+id", (cb, root) -> cb.asc(root.get(TAG_ID)),
-            "-id", (cb, root) -> cb.desc(root.get(TAG_ID))
-    );
+    private final Map<String, BiFunction<CriteriaBuilder, Root<Tag>, javax.persistence.criteria.Order>> sortBy =
+            Map.of("+name", (cb, root) -> cb.asc(root.get(TAG_NAME)),
+                    "-name", (cb, root) -> cb.desc(root.get(TAG_NAME)),
+                    "+id", (cb, root) -> cb.asc(root.get(TAG_ID)),
+                    "-id", (cb, root) -> cb.desc(root.get(TAG_ID)));
 
     @PersistenceContext
     private EntityManager em;
@@ -51,6 +52,33 @@ public class TagRepositoryImpl implements TagRepository {
     @Override
     public void delete(Tag entity) {
         em.remove(entity);
+    }
+
+    @Override
+    public Optional<Tag> findMostPopularTagOfUserWithHighestCostOfAllOrders() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> longCriteriaQuery = cb.createQuery(Long.class);
+        Root<Order> order = longCriteriaQuery.from(Order.class);
+        longCriteriaQuery
+                .select(order.get("user").get("id"))
+                .groupBy(order.get("user").get("id"))
+                .orderBy(cb.desc(cb.sum(order.get("cost"))));
+        Long userId = em.createQuery(longCriteriaQuery)
+                .setMaxResults(1)
+                .getResultList()
+                .get(0);
+        CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
+        Root<Order> orderRoot = cq.from(Order.class);
+        Join<Certificate, Tag> joinedTag = orderRoot.join("certificate")
+                .join("tags");
+        cq.select(joinedTag)
+                .groupBy(joinedTag.get("id"))
+                .orderBy(cb.desc(cb.count(joinedTag.get("id"))))
+                .where(cb.equal(orderRoot.get("user").get("id"), userId));
+        return em.createQuery(cq)
+                .setMaxResults(1)
+                .getResultList().stream()
+                .findFirst();
     }
 
     public List<Tag> findAll(LinkedMultiValueMap<String, String> fields, Pageable pageable) {
@@ -83,15 +111,15 @@ public class TagRepositoryImpl implements TagRepository {
                       Root<Tag> root) {
         String stringSortParams = fields.getOrDefault(SORT_REQUEST_PARAM, List.of(TAG_ID)).get(0).trim();
         if (stringSortParams.isEmpty()) {
-            stringSortParams = "+".concat(TAG_ID);
+            stringSortParams = "+" .concat(TAG_ID);
         }
         List<String> sortParams = Arrays.stream(stringSortParams.split(","))
                 .map(String::trim)
-                .map(el -> el.startsWith(TAG_NAME) ? "+".concat(el) : el)
-                .map(el -> el.startsWith(TAG_ID) ? "+".concat(el) : el)
+                .map(el -> el.startsWith(TAG_NAME) ? "+" .concat(el) : el)
+                .map(el -> el.startsWith(TAG_ID) ? "+" .concat(el) : el)
                 .distinct()
                 .collect(Collectors.toList());
-        List<Order> orders = sortParams.stream()
+        List<javax.persistence.criteria.Order> orders = sortParams.stream()
                 .filter(sortBy::containsKey)
                 .map(tagProperty -> sortBy.get(tagProperty).apply(criteriaBuilder, root))
                 .collect(Collectors.toList());
