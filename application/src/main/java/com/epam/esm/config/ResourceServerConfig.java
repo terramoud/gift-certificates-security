6b10955c8c7a06859fa36826af7bdb51d3a12926
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-07 10:19:38 +0200

    implement ResourceServerConfig

---
+++ application/src/main/java/com/epam/esm/config/ResourceServerConfig.java
@@ -0,0 +1,58 @@
+package com.epam.esm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/tags").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/tags/*").permitAll()
+//                .mvcMatchers(HttpMethod.GET, "/api/v1/tags/^\\d+$/gift-certificates").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/tags/{tagId}/gift-certificates").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/tags/name/{tagName}/gift-certificates").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/gift-certificates").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/gift-certificates/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter defaultConverter = new JwtGrantedAuthoritiesConverter();
+        JwtGrantedAuthoritiesConverter roleConverter = new JwtGrantedAuthoritiesConverter();
+        roleConverter.setAuthoritiesClaimName("roles");
+        roleConverter.setAuthorityPrefix("ROLE_");
+        DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter =
+                new DelegatingJwtGrantedAuthoritiesConverter(roleConverter, defaultConverter);
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return jwtConverter;
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+}
