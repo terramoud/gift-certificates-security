47877aedd15bd1bbc3fa556a622573a16912b9b2
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-07 10:23:36 +0200

    create authority constraint annotations

---
+++ application/src/main/java/com/epam/esm/security/annotations/UserWritePermission.java
@@ -0,0 +1,14 @@
+package com.epam.esm.security.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('SCOPE_write') and hasRole('ROLE_USER')")
+public @interface UserWritePermission {
+}
