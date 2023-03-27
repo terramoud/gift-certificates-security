d05a3e0ae0460f47f41c1593ed7625c40fd1ccfe
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-27 17:43:42 +0300

    add the endpoint to change user password

--- authorization-server/src/main/java/com/epam/esm/controller/UserController.java
+++ authorization-server/src/main/java/com/epam/esm/controller/UserController.java
@@ -1,18 +1,18 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.dto.PasswordDto;
 import com.epam.esm.dto.UserDetailsDto;
 import com.epam.esm.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+import static com.epam.esm.config.ConstantsValidationMessages.USER_INVALID_ID;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -23,8 +23,16 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDetails> create(@RequestBody @Valid UserDetailsDto userDetailsDto) {
-        UserDetails addedUserDetailsDto = userService.create(userDetailsDto);
+    public ResponseEntity<UserDetailsDto> create(@RequestBody @Valid UserDetailsDto userDetailsDto) {
+        UserDetailsDto addedUserDetailsDto = userService.create(userDetailsDto);
+        return new ResponseEntity<>(addedUserDetailsDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{user-id}/password")
+    public ResponseEntity<UserDetailsDto> changePassword(
+            @PathVariable("user-id") @Positive(message = USER_INVALID_ID) Long userId,
+            @RequestBody @Valid PasswordDto changePasswordDto) {
+        UserDetailsDto addedUserDetailsDto = userService.changePassword(userId, changePasswordDto);
         return new ResponseEntity<>(addedUserDetailsDto, HttpStatus.CREATED);
     }
 }
