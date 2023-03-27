83167594317b4710880e1dff9cd6b4498369474a
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-27 17:37:47 +0300

    add findById(@NonNull Long id) Spring Data JPA method

--- authorization-server/src/main/java/com/epam/esm/repository/UserRepository.java
+++ authorization-server/src/main/java/com/epam/esm/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.epam.esm.repository;
 
 import com.epam.esm.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLogin(String login);
+
+    @NonNull
+    Optional<User> findById(@NonNull Long id);
 }
 
