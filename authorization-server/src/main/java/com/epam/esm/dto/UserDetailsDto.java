5383e30931ecec488d85d8dcbd27e1e5c5123222
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-27 17:52:27 +0300

    cancel serialize operation for user password field to JSON and add static method to convert User to DTO

--- authorization-server/src/main/java/com/epam/esm/dto/UserDetailsDto.java
+++ authorization-server/src/main/java/com/epam/esm/dto/UserDetailsDto.java
@@ -2,6 +2,7 @@ package com.epam.esm.dto;
 
 import com.epam.esm.model.entity.Role;
 import com.epam.esm.model.entity.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.EnumType;
@@ -9,19 +10,10 @@ import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import static com.epam.esm.config.ConstantsValidationMessages.*;
+
 @Data
 public class UserDetailsDto {
-
-    public static final String USER_INVALID_LOGIN = "user.invalid.login";
-    public static final String USER_INVALID_EMAIL = "user.invalid.email";
-    public static final String USER_INVALID_PASSWORD = "user.invalid.password";
-    public static final String USER_LOGIN_NULL = "user.login.null";
-    public static final String USER_EMAIL_NULL = "user.email.null";
-    public static final String USER_PASSWORD_NULL = "user.password.null";
-    public static final String USER_ROLE_NULL = "user.role.null";
-    public static final String EMAIL_REGEXP = "^\\w+([.-]?\\w+){0,249}@\\w+([.-]?\\w+){0,249}(\\.\\w{2,3}){1,249}$";
-    public static final String LOGIN_REGEXP = "^[\\p{L}][\\p{L}0-9]{2,31}$";
-    public static final String PASSWORD_REGEXP = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,31}$";
 
     @NotNull(message = USER_LOGIN_NULL)
     @Pattern(regexp = LOGIN_REGEXP, message = USER_INVALID_LOGIN)
@@ -33,6 +25,7 @@ public class UserDetailsDto {
 
     @NotNull(message = USER_PASSWORD_NULL)
     @Pattern(regexp = PASSWORD_REGEXP, message = USER_INVALID_PASSWORD)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
@@ -46,5 +39,14 @@ public class UserDetailsDto {
         user.setPassword(this.password);
         user.setRole(role);
         return user;
+    }
+
+    public static UserDetailsDto toDto(User user) {
+        UserDetailsDto dto = new UserDetailsDto();
+        dto.setLogin(user.getLogin());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        dto.setRole(user.getRole());
+        return dto;
     }
 }
