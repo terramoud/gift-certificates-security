cdf704853d36a4072ed250fef604e0e8bb005ecb
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-03-27 17:36:23 +0300

    add ResourceBundleMessageSource and Translator messages beans

--- authorization-server/src/main/java/com/epam/esm/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/epam/esm/AuthorizationServerApplication.java
@@ -1,7 +1,10 @@
 package com.epam.esm;
 
+import com.epam.esm.exceptions.Translator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,4 +15,22 @@ public class AuthorizationServerApplication {
         SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
+    /**
+     * Set the list of resource files here
+     *
+     * @return resourceBundleMessageSource
+     */
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
+        rs.setBasenames("messages");
+        rs.setDefaultEncoding("UTF-8");
+        rs.setUseCodeAsDefaultMessage(true);
+        return rs;
+    }
+
+    @Bean
+    public Translator translator() {
+        return new Translator(messageSource());
+    }
 }
