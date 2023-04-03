f6df6106248d6e290b88bae6d3c9ab045f602276
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-04-03 15:24:27 +0300

    config datasource for tests using test.properties file instead of hardcoded strings

--- repository-api/src/test/java/com/epam/esm/config/RepositoryTestConfig.java
+++ repository-api/src/test/java/com/epam/esm/config/RepositoryTestConfig.java
@@ -1,69 +1,36 @@
 package com.epam.esm.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.Properties;
-
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan("com.epam.esm.repository")
 @EntityScan("com.epam.esm.domain")
+@EnableJpaRepositories("com.epam.esm.repository.api")
+@PropertySource("classpath:test.properties")
 public class RepositoryTestConfig {
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(
+            @Value("${spring.datasource.hikari.driver-class-name}") String mysqlDriver,
+            @Value("${spring.datasource.username}") String userName,
+            @Value("${spring.datasource.password}") String password,
+            @Value("${spring.datasource.url}") String url) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUsername("root");
-        dataSource.setPassword("ffff");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/gift_certificates");
+        dataSource.setDriverClassName(mysqlDriver);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
         return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em
-                = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.epam.esm");
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
-        return em;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
-
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("spring.jpa.show-sql", "true");
-        properties.setProperty("spring.jpa.properties.hibernate.format_sql", "true");
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.hbm2ddl.auto", "none");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        return properties;
     }
 }
