0a97db0c0890ce6bc417c2e752283ce594019c38
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-04-03 16:09:57 +0300

    update for updated JUnit tests

--- service-impl/src/test/java/com/epam/esm/config/ServiceTestConfig.java
+++ service-impl/src/test/java/com/epam/esm/config/ServiceTestConfig.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan("com.epam.esm.repository")
 @EntityScan("com.epam.esm.domain")
+@EnableJpaRepositories("com.epam.esm.repository.api")
 public class ServiceTestConfig {
+
 }
